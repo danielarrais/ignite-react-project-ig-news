d60dc96d5e8f68eef2ba605c150366eae599683e
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-03-02 15:34:01 -0300

    Created styles for testing sass and css modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Hello Word</h1>
+    <h1 className={styles.title}>
+      Hello <span>Word</span>
+    </h1>
   )
 }
