704735657f011f44d83d25125739b9fe8a246e0d
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-03-02 23:09:06 -0300

    Added favicon icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,900&display=swap" rel="stylesheet" />
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
         </Head>
         <Main />
         <NextScript />
