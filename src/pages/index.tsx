e876b114fef7f77ae743e4528bc15c1cd5baacec
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-04-11 20:02:36 -0300

    Created home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import styles from './index.module.scss';
 
 export default function Home() {
   return (
@@ -6,9 +8,21 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>Word</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img
+          src="/images/avatar.svg"
+          alt="Girl coding"
+        />
+      </main>
     </>
   )
 }
