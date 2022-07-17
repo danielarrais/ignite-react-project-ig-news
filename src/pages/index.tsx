aff149fc5a9e0df9c52b2cf8333536d67e06bb0a
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-17 16:37:06 -0300

    Created subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './index.module.scss';
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img
