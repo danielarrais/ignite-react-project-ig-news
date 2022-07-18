f220624ed5f9c3f239d4c4add90da328ba1d95b2
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-17 21:07:44 -0300

    Added auth-next functions on singn button

--- src/components/SinginButton/index.tsx
+++ src/components/SinginButton/index.tsx
@@ -1,24 +1,26 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { useState } from 'react';
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export const SingInButton = () => {
-  const [isUserLoggedIn, setIisUserLoggedIn] = useState(true);
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button"
       className={styles.singInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#84d361" />
-      Sing in with GitHub
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button
       type="button"
       className={styles.singInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sing in with GitHub
