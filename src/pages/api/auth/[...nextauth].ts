fc7eee6a94c817b4dba25e7a0169acaa754f5fdf
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-30 10:59:49 -0300

    Register users data on faunadb after login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,46 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      authorization:  {
+      authorization: {
         params: {
           scope: 'read:user'
         }
-      }
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email: email } }
+            ),
+            q.Abort
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  },
 })
