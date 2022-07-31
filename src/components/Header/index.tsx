76a6c19d8729a0521d0dc36e3b78f0d76d905310
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-31 14:46:23 -0300

    Added path on home menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
       <div className={styles.headContent}>
         <Image src="/images/logo.svg" alt="ig.news" width='100' height='100'/>
         <nav>
-          <a className={styles.active} href="">Home</a>
+          <a className={styles.active} href="/">Home</a>
           <a href="">Posts</a>
         </nav>
 
