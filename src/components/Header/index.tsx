83475edbef9c816d0951a1ea282d36eb3635d1d8
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-08-07 21:02:17 -0300

    COnfigured link to page posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
         <Image src="/images/logo.svg" alt="ig.news" width='100' height='100'/>
         <nav>
           <a className={styles.active} href="/">Home</a>
-          <a href="">Posts</a>
+          <a href="/posts">Posts</a>
         </nav>
 
         <SingInButton />
