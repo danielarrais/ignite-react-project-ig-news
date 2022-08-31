b998c1fa4b4a8e3168f42641bcc2a83eabb8d87f
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-08-30 22:07:39 -0300

    Store subscription on session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,6 +17,42 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session({ session }) {
+      try {
+        const activeSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: activeSubscription
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        };
+      }
+    },
     async signIn({ user }) {
       const { email } = user;
 
@@ -39,7 +75,7 @@ export default NextAuth({
           )
         );
         return true;
-      } catch(err) {
+      } catch (err) {
         alert(err.message)
         return false;
       }
